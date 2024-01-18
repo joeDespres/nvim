bc242eca5bfe163e6c736f1f6fdf66bb00e66352
joeDespres <100436066+joeDespres@users.noreply.github.com>
2024-01-18 08:56:17 -0500

    Update installnvim.sh

--- installnvim.sh
+++ installnvim.sh
@@ -1,6 +1,6 @@
 # bin/bash
 curl -LO https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
 chmod u+x nvim.appimage && ./nvim.appimage
-sudo apt-get install fuse
+yes | sudo apt-get install fuse
 sudo mv nvim.appimage /usr/local/bin/nvim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
