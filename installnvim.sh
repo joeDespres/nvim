ea6805c4491ee31b9bdf0146cd68217a3e541737
joeDespres <100436066+joeDespres@users.noreply.github.com>
2024-01-18 13:28:12 -0500

    Update installnvim.sh

--- installnvim.sh
+++ installnvim.sh
@@ -1,6 +1,6 @@
 # bin/bash
 curl -LO https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
-chmod u+x nvim.appimage && ./nvim.appimage
+chmod u+x nvim.appimage
 yes | sudo apt-get install fuse
 yes | sudo apt install nodejs
 sudo mv nvim.appimage /usr/local/bin/nvim
