3039d317bc774757312abb394f233c544cbbed0b
joeDespres <100436066+joeDespres@users.noreply.github.com>
2024-01-19 10:39:52 -0500

    Update installnvim.sh

--- installnvim.sh
+++ installnvim.sh
@@ -4,7 +4,7 @@ chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 sudo apt-get install fuse -y
 sudo apt install nodejs -y
-git clone git@github.com:joeDespres/NvChad.git ~/.config/nvim --depth 1
+git clone https://github.com/joeDespres/NvChad.git ~/.config/nvim --depth 1
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
 rm ripgrep_13.0.0_amd64.deb
