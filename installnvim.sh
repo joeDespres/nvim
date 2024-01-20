42e6f3cf46437f104b0a13e50ad4f30fff5f648c
joeDespres <100436066+joeDespres@users.noreply.github.com>
2024-01-20 08:13:18 -0500

    Update installnvim.sh

--- installnvim.sh
+++ installnvim.sh
@@ -1,11 +1,16 @@
 # bin/bash
 curl -LO https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
+curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.4/bottom_0.9.4_amd64.deb
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 sudo apt-get install fuse -y
 sudo apt install nodejs -y
 sudo apt-get install zsh -y
-git clone https://github.com/joeDespres/NvChad.git ~/.config/nvim --depth 1
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i bottom_0.9.4_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
 rm ripgrep_13.0.0_amd64.deb
+rm bottom_0.9.4_amd64.deb
+
+git clone https://github.com/joeDespres/NvChad.git ~/.config/nvim --depth 1
