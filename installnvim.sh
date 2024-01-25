c5f070401f5370e110dfd6a916b0f231df80bab4
joeDespres <100436066+joeDespres@users.noreply.github.com>
2024-01-24 19:12:09 -0500

    Update installnvim.sh

--- installnvim.sh
+++ installnvim.sh
@@ -2,7 +2,8 @@
 curl -LO https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
 curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.4/bottom_0.9.4_amd64.deb
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -1
+cargo install stylua
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 sudo apt-get install fuse -y
