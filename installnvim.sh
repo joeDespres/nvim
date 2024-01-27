3c9904dc636e3dd503b92bed22d99a45bde44618
joeDespres <joe.d@gastrograph.com>
2024-01-26 19:17:08 -0500

    add pyenv

--- installnvim.sh
+++ installnvim.sh
@@ -8,6 +8,7 @@ cargo install stylua
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 sudo apt-get install fuse -y
+sudo apt install python3.8-venv -y
 sudo apt install nodejs -y
 sudo apt-get install zsh -y
 sudo dpkg -i bottom_0.9.4_amd64.deb
